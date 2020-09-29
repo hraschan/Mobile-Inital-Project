7c6d2c44e856d9723a59ea48239165471e562b48
Maximilian Hraschan <mt171109@fhstp.ac.at>
2020-09-29 14:50:56 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Citybus</title>
+</head>
+<body>
+    <div class="header">
+        
+    </div>
+    <div class="content">
+        <h2>CONTENT AREA</h2>
+
+    </div>
+
+</body>
+<footer>
+</footer>
+</html>
